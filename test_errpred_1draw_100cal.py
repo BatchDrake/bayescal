f85e7da1001e7d58a5e2ff5460272efeb862fed0
Gonzalo J. Carracedo Carballal <gcarracedo@cab.inta-csic.es>
2023-10-05 09:39:41 +0200

    Add license statement

--- test_errpred_1draw_100cal.py
+++ test_errpred_1draw_100cal.py
@@ -1,3 +1,33 @@
+#
+# Copyright (c) 2023 Gonzalo J. Carracedo <BatchDrake@gmail.com>
+#
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -185,7 +215,8 @@ if first_run and not FORCE_UNINFORMATIVE:
 
     print(fr'  Done. RMS file saved to classic_rms.npy')
     np.save('classic_rms', clasrms)
-
+    np.savez('alpha', alphas, alpha_mean, alpha_cov)
+    
     if NOBAYES:
         sys.exit(0)
     
@@ -319,4 +350,4 @@ plt.tight_layout()
 
 np.savez('caldata.bin', nn, needed, uncertainty, est, var, rms, Sigma)
 
-plt.show()
+# plt.show()
